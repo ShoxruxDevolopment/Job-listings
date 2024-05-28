a1c2a410db3ed99977aa72a10aa0ae168a376ab2
ShoxruxDevolopment <shoxruxrustamovshoxfitnes@gmail.com>
2024-05-28 11:39:24 +0500

    add card-active and main.js

---
+++ main.js
@@ -0,0 +1,6 @@
+const elCardActiveClass=document.querySelector('.card-active-class');
+const elCardActive=document.querySelector('.card-active');
+const elJobItem=document.querySelector('.job-item-active');
+elJobItem.addEventListener('click', function(){
+  document.elCardActive.classList.add('card-active-class');
+});
