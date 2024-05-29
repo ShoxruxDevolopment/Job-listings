427bc84764353092742618d68e5c45f7778727c6
ShoxruxDevolopment <shoxruxrustamovshoxfitnes@gmail.com>
2024-05-29 09:44:53 +0500

    add js function add and remove

--- main.js
+++ main.js
@@ -1,6 +1,17 @@
-const elCardActiveClass=document.querySelector('.card-active-class');
-const elCardActive=document.querySelector('.card-active');
-const elJobItem=document.querySelector('.job-item-active');
-elJobItem.addEventListener('click', function(){
-  document.elCardActive.classList.add('card-active-class');
+const elCardActive = document.querySelector('.card-active');
+const elJobs = document.querySelector('.js-jobs');
+const elJobLink = elJobs.querySelector('.job-link');
+const elLinkIcons = document.querySelector('.link-icons');
+const elCloser= document.querySelector('.closer')
+
+  elJobLink.addEventListener('click', function(){
+    elCardActive.classList.add('card-active-class');
+
+
+ });
+ elCloser.addEventListener('click', function(){
+  elCardActive.classList.remove('card-active-class');
 });
+  elLinkIcons.addEventListener('click', function(){
+    elCardActive.classList.remove('card-active-class');
+  });
